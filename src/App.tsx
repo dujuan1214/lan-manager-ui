2f314d5ec96b5b862f2507a15f8dc0c617c8d0be
Du <1149464651@qq.com>
2020-04-28 00:48:17 +0800

    feat: 搜索框

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import React, { FC } from "react";
+import Nav from "./main/nav";
 import theme from "./theme";
 
 const App: FC = function (props) {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>LAN Manager UI</div>
+      <div>
+        <Nav></Nav>
+      </div>
     </ThemeProvider>
   );
 };
---
+++ src/main/Nav.tsx
@@ -0,0 +1,37 @@
+import IconButton from "@material-ui/core/IconButton";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import { makeStyles } from "@material-ui/core/styles";
+import MagnifyIcon from "mdi-material-ui/Magnify";
+import React, { FC } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  root: { textAlign: "center" },
+  search: {
+    width: "90%",
+    height: "45px",
+    marginTop: "10px",
+  },
+}));
+const Nav: FC = function (props) {
+  const classes = useStyles(props);
+  return (
+    <nav className={classes.root}>
+      <OutlinedInput
+        color="secondary"
+        fullWidth
+        onChange={() => {}}
+        placeholder="搜索"
+        className={classes.search}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton>
+              <MagnifyIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+    </nav>
+  );
+};
+export default Nav;
